8c10a71689dc20bd438b119f925e9f2f021c743e
LuzNiz <luzniz37@gmail.com>
2023-09-04 20:32:31 -0300

    Subiendo manejo de errores y codigos de estado

--- src/producto/producto.controller.ts
+++ src/producto/producto.controller.ts
@@ -1,21 +1,44 @@
-import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, HttpCode, Res, HttpStatus } from '@nestjs/common';
 import { ProductoService } from './producto.service';
 import { Producto } from './producto.interface';
 
 @Controller('/productos')
 export class ProductoController {
-    constructor(private readonly productoService: ProductoService){}
+    constructor(private readonly productoService: ProductoService) {}
 
-@Get()
-getProducto() :Promise <Producto[]>{
-    return this.productoService.getProducto();
-}
-@Get("/:id")
-getProductoById(@Param("id") id:number){
-    return this.productoService.getProductoById(id);
-}
-@Post()
-crearProducto(@Body() body): Promise <Producto>{
-    return this.productoService.crearProducto(body);
-}
+    @Get()
+    async getProducto(@Res() response): Promise<Producto[]> {
+        try {
+            const responseFromService = await this.productoService.getProductos();
+            if (Object.keys(responseFromService).length) {
+                return response.status(HttpStatus.OK).json(responseFromService);
+            }
+        } catch (error) {
+            return response.status(HttpStatus.NOT_FOUND).json({ message: "Products not found" });
+        }
+    }
+
+    @Get("/:id")
+    async getProductoById(@Res() response, @Param("id") id: number): Promise<Producto> {
+        try {
+            const resposeFromService = await this.productoService.getProductoById(id);
+            if (Object.keys(resposeFromService).length) {
+                return response.status(HttpStatus.OK).json(resposeFromService);
+            }
+        } catch (err) {
+            return response.status(HttpStatus.NOT_FOUND).json({ message: "Product not found" });
+        }
+    }
+    
+    @Post()
+    async crearProducto(@Res() response, @Body() body): Promise<Producto> {
+        try {
+            const responseFromService = await this.productoService.crearProducto(body);
+            if (Object.keys(responseFromService).length) {
+                return response.status(HttpStatus.CREATED).json({ message: 'El recurso ha sido creado con éxito' });
+            }
+        } catch (error) {
+            return response.status(HttpStatus.BAD_REQUEST).json({ message: 'El recurso no pudo ser creado' });
+        }
+    }
 }
--- src/producto/producto.service.ts
+++ src/producto/producto.service.ts
@@ -2,40 +2,61 @@ import { Injectable } from '@nestjs/common';
 import { Producto } from './producto.interface';
 
 const BASE_URL = "http://localhost:3030/productos/";
+
 @Injectable()
 export class ProductoService {
-   async crearProducto(body): Promise <Producto> {
-        const id = await this. setId();
-        const {nombre, marca, categoria, descripcion, precio, talla,img} = body;
-        const nuevoProducto = {id, nombre, marca, categoria, descripcion, precio, talla,img};
-        const res = await fetch(BASE_URL, {
-            method: "POST",
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(nuevoProducto),
-
-        })
-        const parsed =await res.json();
-        return parsed
-      
-    }
-    private async setId(): Promise <number> {
-        const res = await this.getProducto();   
-        const id = res.pop().id + 1;
-        return id;  
-    }
-  async getProductoById(id) {
-    const res = await fetch (BASE_URL + id)    
-    const parsed = await res.json()
-
-    return parsed;  
-   }
-   async getProducto(): Promise <Producto[]> {
-    const res = await fetch (BASE_URL)    
-    const parsed = await res.json()
-
-    return parsed; 
-
+    async crearProducto(body) {
+        try {
+            if (Object.keys(body).length > 1) {
+                const id = await this.setId();
+                const { nombre, marca, categoria, descripcion, precio, talla, img } = body;
+                const nuevoProducto = { id, nombre, marca, categoria, descripcion, precio, talla, img };
+                const res = await fetch(BASE_URL, {
+                    method: "POST",
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(nuevoProducto),
+                })
+                const parsed = await res.json();
+                return parsed;
+            } else {
+                throw new Error('No se ha podido crear el recurso');
+            }
+        } catch (error) {
+            throw new Error(`Se ha producido un error: ${error}`);
         }
+
+    }
+    private async setId(): Promise<number> {
+        const res = await this.getProductos();
+        const id = res.pop().id + 1;
+        return id;
+    }
+
+    async getProductoById(id): Promise <Producto> {
+        try{
+            const res = await fetch(BASE_URL + id);
+            if(!res.ok) {
+                throw new Error (`Error al obtener los datos: ${res.status} - ${res.statusText}`);
+            }
+            const parsed = await res.json();
+            return parsed;
+        }catch (err) {
+            throw new Error (`Error al obtener los datos: ${err.status} - ${err.statusText}`);
+        }
+    }
+
+    async getProductos(): Promise<Producto[]> {
+        try {
+            const res = await fetch(BASE_URL);
+            if (!res.ok) {
+                throw new Error(`Error al obtener los datos: ${res.status} - ${res.statusText}`);
+            }
+            const parsed = await res.json()
+            return parsed;
+        } catch (err) {
+            throw new Error(`Error al obtener los datos: ${err.status} - ${err.statusText}`);
+        }
+    }
 }
