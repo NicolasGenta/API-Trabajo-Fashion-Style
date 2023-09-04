4e5c946943f3375af264453e3197966b831d9481
NicolasGenta <nicolasgenta93@gmail.com>
2023-09-04 20:23:16 -0300

    version 4

--- src/producto/producto.controller.ts
+++ src/producto/producto.controller.ts
@@ -1,21 +1,45 @@
-import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, HttpCode, Res, HttpStatus} from '@nestjs/common';
 import { ProductoService } from './producto.service';
 import { Producto } from './producto.interface';
+import { response } from 'express';
 
 @Controller('/productos')
 export class ProductoController {
     constructor(private readonly productoService: ProductoService){}
 
 @Get()
-getProducto() :Promise <Producto[]>{
-    return this.productoService.getProducto();
+async getProducto(@Res() response) :Promise <Producto[]>{
+    const responseFromService = await this.productoService.getProducto();
+    if(Object.keys(responseFromService).length){
+        return response.status(HttpStatus.OK)
+        }else{
+            return response.status(HttpStatus.NOT_FOUND).json({
+                message:"productos no encontrado",
+            })
+}
 }
 @Get("/:id")
-getProductoById(@Param("id") id:number){
-    return this.productoService.getProductoById(id);
+async getProductoById(@Res() response, @Param("id") id:number){
+    const responseFromService = await this.productoService.getProductoById(id);
+    if(Object.keys(responseFromService).length){
+        return response.status(HttpStatus.OK)
+        }else{
+            return response.status(HttpStatus.NOT_FOUND).json({
+                message:"producto no encontrado",
+            })
+
+    }
 }
 @Post()
-crearProducto(@Body() body): Promise <Producto>{
-    return this.productoService.crearProducto(body);
+async crearProducto(@Res() response, @Body() body): Promise <Producto>{
+    const responseFromService = await this.productoService.crearProducto(body);
+    if(Object.keys(responseFromService).length){
+        return response.status(HttpStatus.OK)
+
+    }else{
+
+        return response.status(HttpStatus.BAD_REQUEST).json({
+            message: "esta mal"
+          })
 }
-}
+}}
--- src/producto/producto.service.ts
+++ src/producto/producto.service.ts
@@ -4,38 +4,57 @@ import { Producto } from './producto.interface';
 const BASE_URL = "http://localhost:3030/productos/";
 @Injectable()
 export class ProductoService {
-   async crearProducto(body): Promise <Producto> {
-        const id = await this. setId();
-        const {nombre, marca, categoria, descripcion, precio, talla,img} = body;
-        const nuevoProducto = {id, nombre, marca, categoria, descripcion, precio, talla,img};
-        const res = await fetch(BASE_URL, {
-            method: "POST",
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(nuevoProducto),
+   async crearProducto(body){
+    try{  
+    if(Object.keys(body).length){
 
-        })
-        const parsed =await res.json();
-        return parsed
+        const id = await this.setId();
+            const {nombre, marca, categoria, descripcion, precio, talla,img} = body;
+            const nuevoProducto = {id, nombre, marca, categoria, descripcion, precio, talla, img};
+            const res = await fetch(BASE_URL, {
+                method: "POST",
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(nuevoProducto),
+    
+            })
+            const parsed =await res.json();
+            return parsed
+    }else{
+        return {}
+    }  }catch(error){
+        return{}
+
+    }
       
     }
     private async setId(): Promise <number> {
+
         const res = await this.getProducto();   
         const id = res.pop().id + 1;
         return id;  
     }
   async getProductoById(id) {
+    try{
     const res = await fetch (BASE_URL + id)    
     const parsed = await res.json()
 
     return parsed;  
+    }catch(error){
+        return {}
+    }
    }
    async getProducto(): Promise <Producto[]> {
-    const res = await fetch (BASE_URL)    
-    const parsed = await res.json()
+    try{
 
-    return parsed; 
+        const res = await fetch (BASE_URL)    
+        const parsed = await res.json()
+        return parsed; 
+    } catch(error){
+        return []
+    }
+
 
         }
 }
