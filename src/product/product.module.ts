d426cbca6edc23b44184da54c42f657aa17921d6
LuzNiz <luzniz37@gmail.com>
2024-07-18 12:09:16 -0300

    18.7

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -15,10 +15,11 @@ import { Pedido } from 'src/entities/pedido.entity';
 import { Pedido_detalle } from 'src/entities/pedido_detalle.entity';
 import { ImageService } from 'src/image/image.service';
 import { ImageModule } from 'src/image/image.module';
+import { Persona } from 'src/entities/persona.entity';
 
 @Module({
     imports: [ImageModule, TypeOrmModule.forFeature([Products, Category, Emprendimiento, Rubro, Usuario,
-    Cliente, Estado, Pedido, Pedido_detalle])],
+    Cliente, Estado, Pedido, Pedido_detalle, Persona])],
     providers: [ProductService, PedidosService],
     controllers: [ProductController, PedidosController]
 })
