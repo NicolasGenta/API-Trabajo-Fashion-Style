72db6f91dee4ea92d98bc03f05cef6da51e24b48
LuzNiz <luzniz37@gmail.com>
2024-07-18 14:37:09 -0300

    Subiendo modificacion path para guardado de imagenes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+import * as express from 'express';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }),
